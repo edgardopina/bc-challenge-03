c7b79ed9d8ce9bcc96f59da5b15d3f8f6b1abcc0
edgardopina <edgardopina@yahoo.com>
2021-08-12 15:47:00 -0600

    Push to develop

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,7 @@ var superSetOfChars = []; // super set of charcaters that merges the sets of cha
 // criteria object
 var criteria = [
 	{
-		name: "Lower Case", // criteria name
+		name: "LOWER-CASE", // criteria name
 		selected: false, //criteria was selected for password (true/false)
 		// define set of charcters for this criteria
 		setOfChars: [
@@ -41,7 +41,7 @@ var criteria = [
 		],
 	},
 	{
-		name: "Upper Case",
+		name: "UPPER-CASE",
 		selected: false,
 		setOfChars: [
 			"A",
@@ -73,12 +73,12 @@ var criteria = [
 		],
 	},
 	{
-		name: "Numeric",
+		name: "NUMBER",
 		selected: false,
 		setOfChars: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
 	},
 	{
-		name: "Special Characters",
+		name: "SPECIAL",
 		selected: false,
 		setOfChars: [
 			"!",
@@ -112,19 +112,16 @@ var presentPrompts = function () {
 	console.log("Inside presentPrompts");
 	superSetOfChars = []; // initializes superSet each time that we present prompts
 	var noneSelected = true; // assumption that users did NOT select ANY criteria
-	// loops to present all password criteria for user selection
+	// presents all password criteria for user selection
 	for (var i = 0; i < NUMBER_OF_CRITERIA; i++) {
-		// captures criteria selection YES/NO - OK/CANCEL - true/false
 		criteria[i].selected = window.confirm(
-			"Do you want to include " + criteria[i].name + " as a criteria to create your password?"
+			"Do you want to include " + criteria[i].name + " characters as a criteria to create your password?"
 		);
 		// if at least one user input is Yes/True then noneSelected assumption is false
 		if (criteria[i].selected == YES) {
-			noneSelected = false; //noneSelected assumption is false, AT LEAST ONE criteria was selected
+			noneSelected = false;
 			// builds superset with selected criteria's setOfChar
 			superSetOfChars = [...superSetOfChars, ...criteria[i].setOfChars];
-			console.log("set i=", i, " ", criteria[i].setOfChars);
-			console.log("Super= ", superSetOfChars);
 		}
 	}
 	// if assumption "that no criteria was selected" is true, send error message and prompt again
@@ -136,7 +133,6 @@ var presentPrompts = function () {
 
 // gets and validates password lenght from user
 var promptPasswordLength = function () {
-	console.log("Inside promptPasswordLength");
 	const PWD_LENGTH_MIN = 8; // const values to validate a valid password length range
 	const PWD_LENGTH_MAX = 128;
 	// read password length
@@ -155,41 +151,33 @@ var promptPasswordLength = function () {
 		window.alert("Please enter a valid response.");
 		promptPasswordLength();
 	}
-	console.log(passwordLength);
 };
 
 // builds the password
 var buildPassword = function () {
-	console.log("inside buildPassword");
-	var password = ""; // declares empty password string
-	var superSetLength = superSetOfChars.length; // length of superSetOfChars
-	console.log(password);
-	console.log(superSetLength);
+	var password = "";
+	var superSetLength = superSetOfChars.length;
 	// loops to add one random character at a time to password
 	for (var i = 0; i < passwordLength; ++i) {
-		var superSetIndex = Math.floor(Math.random() * superSetLength); // random index in range [0,superSetLength]
-		password += superSetOfChars[superSetIndex]; // adds ONE random character from superSetOfChars to password
-		console.log(i, password);
+		password += superSetOfChars[Math.floor(Math.random() * superSetLength)];
 	}
-	return password; // returns built password
+	return password;
 };
 
 // full cycle to generate password
 function generatePassword() {
-	console.log("Inside generatePassword");
-	presentPrompts(); // present and validate prompts for criteria to be used in the password, also builds superSetOfChar
+	presentPrompts(); // present and validate prompts for criteria selection; also builds superSetOfChar
 	promptPasswordLength(); // present and validate prompt for password length
-	var password = buildPassword(); // builds password with superSetOfChars built with selected criteria's setOfChar
+	var password = buildPassword(); // builds password from superSetOfChars
 	return password;
 }
 
-// Write password to the #password  input
+// Write password to the #password input
 function writePassword() {
 	var password = ""; // initializes password string after each run
-	var passwordText = document.querySelector("#password"); // maps variable passwordText to html element id="password"
-	passwordText.value = password; // cleans html id="password" content from previous password
+	var passwordText = document.querySelector("#password"); // maps variable  to html element
+	passwordText.value = password; // cleans screen html id="password" content from previous password
 	password = generatePassword(); // generate password
-	//passwordText = document.querySelector("#password"); // locates element with id="password"
 	passwordText.value = password; // updates html id="password" content with the built password
 }
 
